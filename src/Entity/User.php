b268b566f810c218c5784fb115672eaf8161ff9e
bernardxyz <bernardberisaxyz@gmail.com>
2021-03-05 17:07:07 +0100

    Commit

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Repository\PostRepository;
+use App\Repository\UserRepository;
 use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ORM\Entity(repositoryClass=PostRepository::class)
+ * @ORM\Entity(repositoryClass=UserRepository::class)
  */
 class User implements UserInterface
 {
@@ -20,6 +20,22 @@ class User implements UserInterface
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
 
     /**
      * @ORM\Column(type="string", length=30)
@@ -40,16 +56,6 @@ class User implements UserInterface
      * @ORM\Column(type="boolean")
      */
     private $sex;
-
-    /**
-     * @ORM\Column(type="string", length=200, unique=true)
-     */
-    private $email;
-
-    /**
-     * @ORM\Column(type="string", length=255, unique=true)
-     */
-    private $password;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\City", inversedBy="user")
@@ -76,15 +82,10 @@ class User implements UserInterface
      */
     private $userRace;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $username;
-
     public function __construct()
     {
         $this->userCheckPoint = new ArrayCollection();
-        $this->userType = new ArrayCollection();
+        //$this->userType = new ArrayCollection();
         $this->userRace = new ArrayCollection();
     }
 
@@ -93,14 +94,90 @@ class User implements UserInterface
         return $this->id;
     }
 
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
     public function getFirstName(): ?string
     {
         return $this->firstName;
     }
 
-    public function setFirstName(string $value): self
+    public function setFirstName(string $firstName): self
     {
-        $this->firstName = $value;
+        $this->firstName = $firstName;
 
         return $this;
     }
@@ -110,16 +187,11 @@ class User implements UserInterface
         return $this->lastName;
     }
 
-    public function setLastName(string $value): self
+    public function setLastName(string $lastName): self
     {
-        $this->lastName = $value;
+        $this->lastName = $lastName;
 
         return $this;
-    }
-
-    public function getFullName(): ?string
-    {
-        return $this->firstName.' '. $this->lastName;
     }
 
     public function getBirthDate(): ?DateTimeInterface
@@ -144,45 +216,6 @@ class User implements UserInterface
         $this->sex = $sex;
 
         return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    public function getRoles()
-    {
-        // TODO: Implement getRoles() method.
-    }
-
-    public function getSalt()
-    {
-        // TODO: Implement getSalt() method.
-    }
-
-    public function eraseCredentials()
-    {
-        // TODO: Implement eraseCredentials() method.
     }
 
     public function getUserCheckPoint(): ArrayCollection
@@ -303,18 +336,6 @@ class User implements UserInterface
     public function setCity(?City $city): self
     {
         $this->city = $city;
-
-        return $this;
-    }
-
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
 
         return $this;
     }
